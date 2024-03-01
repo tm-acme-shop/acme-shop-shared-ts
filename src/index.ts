224d14502b6b7ab2cea2c6cfa51d2c4d09be2602
Taber Miyauchi <tabermiyauchi_@Tabers-MacBook-Pro.local>
2024-03-01 10:00:00 -0700

    API-210: Deprecate UserV1 exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+// API-210: DEPRECATED - UserV1 exports scheduled for removal
+// TODO(TEAM-FRONTEND): Remove UserV1, toUserV1, fromUserV1 exports in v3.0
 export * from './models';
 export * from './api';
 export * from './utils';
